9ee4b07ef70dea85decef27a83c400fbd70b6c48
Geraldo Mendes <92949630+Geraldomendes@users.noreply.github.com>
2022-10-13 16:45:29 -0300

    Questões prática 02, 03 e 04

---
+++ Exercicios/AtividadePratica2.java
@@ -0,0 +1,15 @@
+public class AtividadePratica2 {
+
+	public static void main(String[] args) {
+		int num1 = 9;
+		int num2 = 10;
+		int num3 = 5;
+		
+		int soma = num1 + num2 + num3;
+		int media = soma / 3;
+		
+		System.out.printf("A soma das variáveis é: %d", soma);
+		System.out.printf("\nA média das variáveis é: %d", media);
+		
+	}
+}
---
+++ Exercicios/AtividadePratica3.java
@@ -0,0 +1,12 @@
+public class AtividadePratica3 {
+
+	public static void main(String[] args) {
+		int larg = 10;
+		int comp = 5;
+		
+		int area = larg * comp;
+		
+		System.out.printf("A área do retangul é: %d m²", area);
+	}
+	
+}
